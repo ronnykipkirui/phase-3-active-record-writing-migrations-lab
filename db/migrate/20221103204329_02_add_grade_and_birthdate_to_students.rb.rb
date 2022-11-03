9cbab64f4216c7fc774d8ef07f08bbb2af42bdcf
Ronny <ronnybet41@gmail.com>
2022-11-03 23:45:15 +0300

    completed assignment

---
+++ db/migrate/20221103204329_02_add_grade_and_birthdate_to_students.rb.rb
@@ -0,0 +1,6 @@
+class 02AddGradeAndBirthdateToStudents.rb < ActiveRecord::Migration[6.1]
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+end
